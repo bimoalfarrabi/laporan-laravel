f456206817947f84c36d6d7700875c66a789de7e
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-10-23 12:18:05 +0700

    feat: add user roles and seeder for inital users

---
+++ database/seeders/RoleUserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class RoleUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Membuat user dengan role 'super_admin'
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@example.com',
+            'password' => Hash::make('password123'), // Ganti dengan password yang diinginkan
+            'role' => 'superadmin',
+        ]);
+
+        // Membuat user dengan role 'danru'
+        User::create([
+            'name' => 'Danru A',
+            'email' => 'danrua@example.com',
+            'password' => Hash::make('password123'), // Ganti dengan password yang diinginkan
+            'role' => 'danru',
+        ]);
+
+        // Membuat user dengan role 'anggota'
+        User::create([
+            'name' => 'Anggota 1',
+            'email' => 'anggota1@example.com',
+            'password' => Hash::make('password123'), // Ganti dengan password yang diinginkan
+            'role' => 'anggota',
+        ]);
+
+        User::create([
+            'name' => 'Anggota 2',
+            'email' => 'anggota2@example.com',
+            'password' => Hash::make('password123'), // Ganti dengan password yang diinginkan
+            'role' => 'anggota',
+        ]);
+    }
+}
