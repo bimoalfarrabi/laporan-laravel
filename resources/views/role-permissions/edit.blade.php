87deab1027125b6bea6d4c7fa629f2aa2e4846fb
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-10-29 08:19:02 +0700

    fix(ui): Add spacing between role-permission edit buttons

--- resources/views/role-permissions/edit.blade.php
+++ resources/views/role-permissions/edit.blade.php
@@ -44,7 +44,7 @@
                             <x-primary-button>
                                 {{ __('Simpan') }}
                             </x-primary-button>
-                            <a href="{{ route('role-permissions.index') }}" class="ms-4 inline-flex items-center px-4 py-2 bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest hover:bg-gray-300">Batal</a>
+                            <a href="{{ route('role-permissions.index') }}" class="ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Batal</a>
                         </div>
                     </form>
 
