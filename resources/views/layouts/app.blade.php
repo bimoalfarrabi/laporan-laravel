408312a3c9a8a3168ffef545b3bc38cbfc83e221
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-10-30 09:39:43 +0700

    feat: add a footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,11 @@
                 {{ $slot }}
             </main>
         </div>
+        <footer class="bg-white shadow py-2 mt-4">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-500 text-sm">
+                &copy; {{ date('Y') }} Rumah Sakit Blambangan. All rights reserved.
+            </div>
+        </footer>
         @stack('scripts')
     </body>
 </html>
