6d2b422aaafade0a506a8877a8062a6844191254
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-11-03 11:41:08 +0700

    refactor: hide export pdf button on report detail page

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -174,12 +174,12 @@
                                         {{ __('Edit') }}
                                     </a>
                                 @endcan
-                                @can('view', $report) {{-- Use view policy to check if user can export --}}
+                                {{-- @can('view', $report)
                                     <a href="{{ route('reports.exportPdf', $report->id) }}"
                                         class="inline-flex items-center px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-500 focus:bg-purple-500 active:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                         {{ __('Export PDF') }}
                                     </a>
-                                @endcan
+                                @endcan --}}
                             @endif
                             <a href="{{ route('reports.index') }}"
                                 class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
