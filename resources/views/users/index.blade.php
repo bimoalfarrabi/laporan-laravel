5448c699888f61fc71dde2ec31205f35c0304d12
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-11-05 13:05:26 +0700

    fix: Resolve display bug in users index table
    This commit fixes a display issue in the users index page where the table was not rendering correctly.
    The bug was caused by an incomplete @php block in esources/views/users/index.blade.php, leading to raw Blade directives being displayed instead of the formatted table.
    
    Changes include:
    - Corrected the <thead> section in esources/views/users/index.blade.php to properly close the @php block and restore the table structure.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -82,15 +82,122 @@
                                 <thead class="bg-gray-50">
                                     <tr>
                                         @php
-                                        $columns = [
-                                        'id' => 'ID',
-                                        'name' => 'Nama',
-                                        'username' => 'Username',
-                                        'email' => 'Email',
-                                        'nik' => 'NIK',
-                                        'phone_number' => 'No. HP',
-                                        'created_at' => 'Waktu Dibuat',
-                                        'last_login_at' => 'Terakhir Login',
+                                            $columns = [
+                                                'id' => 'ID',
+                                                'name' => 'Nama',
+                                                'username' => 'Username',
+                                                'email' => 'Email',
+                                                'nik' => 'NIK',
+                                                'phone_number' => 'No. HP',
+                                                'created_at' => 'Waktu Dibuat',
+                                                'last_login_at' => 'Terakhir Login',
+                                            ];
+                                        @endphp
+
+                                        @foreach ($columns as $column => $title)
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                <a href="{{ route('users.index', [
+                                                    'sort_by' => $column,
+                                                    'sort_direction' => $sortBy == $column && $sortDirection == 'asc' ? 'desc' : 'asc',
+                                                    'search' => $search,
+                                                    'role' => $filterRole,
+                                                ]) }}">
+                                                    {{ $title }}
+                                                    @if ($sortBy == $column)
+                                                        @if ($sortDirection == 'asc')
+                                                            <span>&#9650;</span>
+                                                        @else
+                                                            <span>&#9660;</span>
+                                                        @endif
+                                                    @endif
+                                                </a>
+                                            </th>
+                                        @endforeach
+
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Peran (Spatie)
+                                        </th>
+
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Aksi
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach ($users as $user)
+                                        <tr>
+                                            <td class="sticky left-0 bg-white px-6 py-4 border-r">
+                                                {{ $user->id }}
+                                            </td>
+                                            <td class="sticky left-16 bg-white px-6 py-4 border-r">
+                                                {{ $user->name }}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                {{ $user->username }}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {{ $user->email }}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {{ $user->nik }}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {{ $user->phone_number }}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {{ $user->roles->pluck('name')->join(', ') }}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <x-waktu-dibuat :date="$user->created_at" />
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                @if ($user->last_login_at)
+                                                    <x-waktu-dibuat :date="$user->last_login_at" />
+                                                @else
+                                                    Belum pernah login
+                                                @endif
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                                <a href="{{ route('users.show', $user->id) }}"
+                                                    class="text-indigo-600 hover:text-indigo-900 mr-2">Show
+                                                </a>
+                                                @can('update', $user)
+                                                    <a href="{{ route('users.edit', $user->id) }}"
+                                                        class="text-blue-600 hover:text-blue-900 mr-2">Edit
+                                                    </a>
+                                                @endcan
+                                                @can('resetPassword', $user)
+                                                    {{-- Tombol Reset Password --}}
+                                                    <form action="{{ route('users.resetPassword', $user->id) }}"
+                                                        method="POST" class="inline">
+                                                        @csrf
+                                                        <button type="submit"
+                                                            class="text-yellow-600 hover:text-yellow-900 mr-2"
+                                                            data-confirm-dialog="true"
+                                                            data-swal-title="Reset Password?"
+                                                            data-swal-text="Password untuk {{ $user->name }} akan direset menjadi '123456'."
+                                                            >Reset
+                                                            Pass</button>
+                                                    </form>
+                                                @endcan
+                                                @can('delete', $user)
+                                                    <form action="{{ route('users.destroy', $user->id) }}" method="POST"
+                                                        class="inline">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="text-red-600 hover:text-red-900"
+                                                            data-confirm-dialog="true"
+                                                            data-swal-title="Hapus Pengguna?"
+                                                            data-swal-text="Pengguna akan dipindahkan ke arsip. Anda yakin?">Hapus</button>
+                                                    </form>
+                                                @endcan
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
                             </table>
                         </div>
                     @endif
