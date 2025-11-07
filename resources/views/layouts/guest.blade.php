22c5c2e8bc0f425ffdd5740c734f421db2cd4607
Muhammad Bimo Habib Al-Farrabi <bimoalfarrabi24@gmail.com>
2025-11-07 10:16:16 +0700

    refactor: make login page more mobile friendly

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,8 +15,8 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div class="w-full">
+        <div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div class="w-full px-4 sm:px-0">
                 <a href="/" class="flex justify-center">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
@@ -25,7 +25,7 @@
                 </div>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-4 sm:px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
