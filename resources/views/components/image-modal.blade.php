df037efd2463637449837c75653dd13832354eed
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2025-11-14 19:12:12 +0700

    fix: Increase spacing for image rotation buttons in modal
    Adjusted the positioning of the image rotation buttons in the modal to provide more space between them and the image, improving visual clarity and user interaction.

--- resources/views/components/image-modal.blade.php
+++ resources/views/components/image-modal.blade.php
@@ -33,16 +33,16 @@
         x-transition:leave-end="opacity-0 transform scale-95"
         class="relative z-10 flex flex-col items-center"
     >
-        <div class="flex items-center justify-center w-full">
-            <button @click="rotation -= 90" class="text-white bg-gray-800 rounded-full p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition mx-2">
+        <div class="relative">
+            <img :src="imageUrl" alt="Image" class="max-w-[90vw] max-h-[80vh] object-contain rounded-lg shadow-lg" :style="{ transform: `rotate(${rotation}deg)` }">
+
+            <button @click="rotation -= 90" class="absolute top-1/2 -translate-y-1/2 left-4 text-white bg-gray-800/75 rounded-full p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 19.5L3 15m0 0l4.5-4.5M3 15h13.5a6 6 0 000-12H3" />
                 </svg>
             </button>
 
-            <img :src="imageUrl" alt="Image" class="max-w-[90vw] max-h-[80vh] object-contain rounded-lg shadow-lg" :style="{ transform: `rotate(${rotation}deg)` }">
-
-            <button @click="rotation += 90" class="text-white bg-gray-800 rounded-full p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition mx-2">
+            <button @click="rotation += 90" class="absolute top-1/2 -translate-y-1/2 right-4 text-white bg-gray-800/75 rounded-full p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 19.5L21 15m0 0l-4.5-4.5M21 15H7.5a6 6 0 010-12H21" />
                 </svg>
