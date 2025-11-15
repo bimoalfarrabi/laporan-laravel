e4902179d717a7498100964c4544bf891f99a612
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2025-11-15 08:01:57 +0700

    feat: add reset button to phone number search

--- resources/views/phone-numbers/index.blade.php
+++ resources/views/phone-numbers/index.blade.php
@@ -25,6 +25,10 @@
                                 class="ml-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
                                 Cari
                             </button>
+                            <a href="{{ route('phone-numbers.index') }}"
+                                class="ml-2 inline-flex items-center px-4 py-2 bg-gray-200 border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                Reset
+                            </a>
                         </div>
                     </form>
 
