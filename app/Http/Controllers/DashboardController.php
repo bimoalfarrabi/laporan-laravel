38896c0d7e85958061272adcd9e318792c4e1ae5
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2025-11-17 11:22:10 +0700

    refactor(dashboard): Remove attendance-related code from DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,11 +18,6 @@ class DashboardController extends Controller
         $user = Auth::user();
         $viewData = [];
 
-        // Fetch today's attendance for the user
-        $viewData['todayAttendance'] = \App\Models\Attendance::where('user_id', $user->id)
-            ->whereDate('time_in', today())
-            ->first();
-
         $viewData['announcements'] = Announcement::with('user')
             ->where(function ($query) {
                 $query->whereNull('starts_at')->orWhere('starts_at', '<=', now());
