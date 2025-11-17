b7810363bc80f08e77fac3c9d6d1512d43dc0f8c
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2025-11-17 08:13:01 +0700

    refactor: deletes "daftar pengumuman" header

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -10,7 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="flex items-center justify-between mb-4">
-                        <h3 class="text-lg font-semibold">Daftar Pengumuman</h3>
                         <div class="flex items-center space-x-4">
                             <a href="{{ route('announcements.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">Buat Pengumuman Baru</a>
                             <a href="{{ route('announcements.archive') }}" class="inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-400 focus:bg-yellow-400 active:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Lihat Arsip Pengumuman</a>
